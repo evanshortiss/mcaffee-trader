08d0cfdd62ae540481673aa56ff37ba3b2bf8841
Evan Shortiss <evanshortiss@gmail.com>
2017-12-24 02:40:47 -0600

    update twitter file to be included

---
+++ twitter.d.ts
@@ -0,0 +1,44 @@
+
+declare module 'twitter' {
+  import * as net from 'net'
+
+  namespace t {
+    class Twitter {
+      constructor (options: ClientOptions)
+
+      stream(path: string, options: StreamOptions): net.Socket
+    }
+
+    interface StreamOptions {
+      [key: string]: any
+    }
+
+    interface ClientOptions {
+      consumer_key: string
+      consumer_secret: string
+      access_token_key: string
+      access_token_secret: string
+    }
+
+    interface StreamData {
+      created_at: string
+      id: number
+      id_str: string
+      text: string
+      truncated: boolean
+      in_reply_to_status_id: null|string
+      in_reply_to_status_id_str: null|string
+      in_reply_to_user_id: number
+      in_reply_to_user_id_str: string
+      in_reply_to_screen_name: string
+      user: {
+        id: number
+      },
+      timestamp_ms: string
+    }
+  }
+
+  function t (opts: t.ClientOptions): t.Twitter
+
+  export = t
+}
